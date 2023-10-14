4658c072f59ff0ef563b4b76223335df76684db7
syarifrm <smaylanno@gmail.com>
2023-10-15 06:05:31 +0700

    Pertemuan 4

---
+++ application/views/v_about.php
@@ -0,0 +1,25 @@
+<section> 
+    <h1><?php echo $judul ?></h1>
+    
+    <h4>Nama</h4> 
+    <ul type="disc"> 
+        <li>Nama Depan : Syarif</li> 
+        <li>Nama Belakang : Maylanno</li> 
+    </ul> 
+    <br> 
+    <h4>Alamat</h4> 
+    <ul type="none"> 
+        <li> Taman Wanasari Indah Blok A7/12A</li> 
+    </ul> 
+    
+    <h4>Tempat Lahir</h4> 
+    <ul type="none"> 
+        <li>Yogyakarta</li> 
+    </ul> 
+    
+    <h4>Olah Raga Favorit</h4> 
+    <ul type="square"> 
+        <li>Bulutangkis</li> 
+        <li>Catur</li> 
+    </ul> 
+</section>
--- application/views/view-latihan1.php
+++ application/views/view-latihan1.php
@@ -5,7 +5,7 @@
 </head> 
 
 <body> 
-    
+
     Halo Kawan.. Yuk kita belajar web programming..!!!<br> 
     Nilai 1 = <?= $nilai1; ?> <br>
     Nilai 2 = <?= $nilai2; ?> <br>
